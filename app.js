93b769ab4fc4f00a69ab1235ed5624dde7a36ee6
Daniel Trone <royhobbstn@yahoo.com>
2016-06-08 15:26:35 +0000

    post limit decreased

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-app.use(bodyParser.json({limit: '20mb'}));
-app.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
+app.use(bodyParser.json({limit: '2mb'}));
+app.use(bodyParser.urlencoded({limit: '2mb', extended: true}));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
